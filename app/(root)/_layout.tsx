fb33aee44c42bc6d6208092632bea5e3ae4f9eda
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-03-21 09:38:43 +0300

    Add AppLayout component for loading state and redirection based on authentication

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Redirect, Slot } from 'expo-router';
+import { ActivityIndicator, SafeAreaView } from 'react-native';
+
+import { useGlobalContect } from '~/lib/global-provider';
+
+export default function AppLayout() {
+  const { loading, isLoggedIn } = useGlobalContect();
+
+  if (loading) {
+    return (
+      <SafeAreaView className="h-full items-center justify-center bg-white">
+        <ActivityIndicator className="text-primary-300" size="large" />
+      </SafeAreaView>
+    );
+
+    if (!isLoggedIn) return <Redirect href="/sign-in" />;
+  }
+
+  return <Slot />;
+}
