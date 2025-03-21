a2991a885c57f4dd2508bd7cbd813af70fe424f2
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-03-21 09:38:35 +0300

    Refactor RootLayout to include custom fonts and global provider; implement splash screen handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,39 @@
-import '../global.css';
+import 'global.css';
 
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+import GlobalProvider from '~/lib/global-provider';
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'Rubik-Bold': require('@/assets/fonts/Rubik-Bold.ttf'),
+    'Rubik-ExtraBold': require('@/assets/fonts/Rubik-ExtraBold.ttf'),
+    'Rubik-Light': require('@/assets/fonts/Rubik-Light.ttf'),
+    'Rubik-Medium': require('@/assets/fonts/Rubik-Medium.ttf'),
+    'Rubik-Regular': require('@/assets/fonts/Rubik-Regular.ttf'),
+    'Rubik-SemiBold': require('@/assets/fonts/Rubik-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
   );
 }
