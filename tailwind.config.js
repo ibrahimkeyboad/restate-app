96d1c3ff0cbd19853cab8533c44e3755dd585577
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-03-21 09:38:23 +0300

    Enhance Tailwind configuration with custom font families and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik-Regular', 'sans-serif'],
+        'rubik-bold': ['Rubik-Bold', 'sans-serif'],
+        'rubik-medium': ['Rubik-Medium', 'sans-serif'],
+        'rubik-light': ['Rubik-Light', 'sans-serif'],
+        'rubik-extrabold': ['Rubik-ExtraBold', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          100: '#0061ff0a',
+          200: '#0061ff1a',
+          300: '#0061ff',
+        },
+        accent: {
+          100: '#fbfbfd',
+        },
+        black: {
+          DEFAULT: '#000000',
+          100: '#8C8E98',
+          200: '#666876',
+          300: '#191D31',
+        },
+        danger: '#f75555',
+      },
+    },
   },
   plugins: [],
 };
