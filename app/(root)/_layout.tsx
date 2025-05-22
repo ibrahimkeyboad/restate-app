df8359e21c84535910f5fb8795f53526f3195583
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-05-22 12:47:56 +0300

    Remove commented-out authentication logic from AppLayout

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -4,16 +4,18 @@ import { ActivityIndicator, SafeAreaView } from 'react-native';
 import { useGlobalContect } from '~/lib/global-provider';
 
 export default function AppLayout() {
-  const { loading, isLoggedIn } = useGlobalContect();
+  // const { loading, isLoggedIn } = useGlobalContect();
 
-  if (loading) {
-    return (
-      <SafeAreaView className="h-full items-center justify-center bg-white">
-        <ActivityIndicator className="text-primary-300" size="large" />
-      </SafeAreaView>
-    );
-  }
-  if (!isLoggedIn) return <Redirect href="/sign-in" />;
+  // if (loading) {
+  //   return (
+  //     <SafeAreaView className="h-full items-center justify-center bg-white">
+  //       <ActivityIndicator className="text-primary-300" size="large" />
+  //     </SafeAreaView>
+  //   );
+  // }
+  // if (!isLoggedIn) return <Redirect href="/sign-in" />;
 
-  return <Slot />;
+  // return <Slot />;
+
+  return()
 }
